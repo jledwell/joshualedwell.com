584cd3d6eff737de004796f99ca71d162f74038c
jledwell <mail@joshualedwell.com>
2013-10-01 18:20:41 -0400

    Initial commit with 2010 files

---
+++ js/site.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	$('#switcher .button').hover(function() {
+		$(this).addClass('over');
+	},
+		function() {
+		$(this).removeClass('over');
+	});
+	$('#switcher .button').click(function() {
+		$('body').removeClass();
+		$('div#mainImage').removeClass();
+		if (this.id == 'swooshy') {
+			$('body').addClass('image1');
+			$('div#mainImage').addClass('grid_8 bg1');
+		}
+		else if (this.id == 'yellow') {
+			$('body').addClass('image2');
+			$('div#mainImage').addClass('grid_8 bg2');
+		}
+		else if (this.id == 'pointy') {
+			$('body').addClass('image3');
+			$('div#mainImage').addClass('grid_8 bg3');
+		}
+		$('#switcher .button').removeClass('selected');
+		$(this).addClass('selected');
+	});
+});
+
+	
+	
